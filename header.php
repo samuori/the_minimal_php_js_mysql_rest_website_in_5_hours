4e8d00b2868b6313769875da862334a156882ad3
Riccardo Serafini <tapion@tapion.it>
2018-05-17 16:47:50 +0200

    bulma CSS e meta viewport per mobile

--- header.php
+++ header.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
 ?>
-
+<!DOCTYPE html>
 <html>
-<head>
-    <title>Sito PHP/JS/HTML/REST</title>
-    <meta charset="utf-8">
-</head>
-
-<body>
-
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8">
+        <title>Sito PHP/JS/HTML/REST</title>
+        <link rel="stylesheet" href="assets/bulma.min.css">
+    </head>
+  <body>
